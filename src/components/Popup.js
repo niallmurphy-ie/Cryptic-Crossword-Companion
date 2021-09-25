fe368ace2f1bfb04557c8f730c6979c627138157
Niall Murphy <leturf@gmail.com>
2021-09-25 15:40:57 +0700

    Working on popup input

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,25 +1,27 @@
-import React, { useState } from "react";
-import Clue from "./Clue";
+import React, { useState, useEffect } from 'react';
+import Clue from './Clue';
 
 function Popup() {
-  const [input, setInput] = useState("");
-  const [fakedClues, setFakedClues] = useState(null);
-  const fakedCurrentActive = "popUpSearch";
+    const [input, setInput] = useState('');
+    const [fakedClues, setFakedClues] = useState(null);
+    const fakedCurrentActive = 'popUpSearch';
 
-  const handleChange = (event) => {
-    setInput(event.target.value);
-  };
+    const handleChange = (event) => {
+        setInput(event.target.value);
+    };
 
-  // Create fake object to use Clue component
-  useEffect(() => {}, [input]);
+    // Create fake object to use Clue component
+    useEffect(() => {
+        setFakedClues([{ popUpSearch: input }]);
+    }, [input]);
 
-  return (
-    <div>
-      <div>Type a clue:</div>
-      <input value={input} onChange={handleChange}></input>
-      <Clue clues={fakedClues} currentActive={fakedCurrentActive} />
-    </div>
-  );
+    return (
+        <div>
+            <div>Type a clue:</div>
+            <input value={input} onChange={handleChange}></input>
+            <Clue clues={fakedClues} currentActive="popUpSearch" />
+        </div>
+    );
 }
 
 export default Popup;
