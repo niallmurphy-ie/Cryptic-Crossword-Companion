cc76b730a35be035409bd5eaa9d302421768f3d6
Niall Murphy <leturf@gmail.com>
2021-09-25 19:23:05 +0700

    Synonyms working in console.log

--- src/utils/getSynonyms.js
+++ src/utils/getSynonyms.js
@@ -1,25 +1,36 @@
 import axios from 'axios';
 import ignorewords from '../data/ignorewords';
+import { clueLength } from './parseClues/helperFunctions';
 
 const getSynonyms = async (clue) => {
     const words = filterWords(clue);
+	 console.log(words);
     const synonyms = {};
     const requests = [];
     requests.push(
         words.forEach(async (word) => {
-            synonyms[word] = await synonymsAPI(word);
+            synonyms[word] = filterReturnedSynonyms(await synonymsAPI(word), clue);
         })
     );
     Promise.all(requests).then(console.log(synonyms));
 };
 
-const filterReturnedSynonyms = (synonyms, clue) => {};
+const filterReturnedSynonyms = (synonyms, clue) => {
+	if (!synonyms) return;
+	const cLength = clueLength(clue);
+	console.log(`Clue Length: ${cLength}`)
+	return synonyms.filter(synonym => synonym.length < cLength + 1);
+};
 
 const synonymsAPI = async (word) => {
-    const response = await axios.get(
-        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-    );
+    const response = await axios
+        .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .catch((error) => {
+            return;
+        });
+
     let synonyms = [];
+    if (!response) return;
     response.data[0].meanings.forEach((meaning) => {
         meaning.definitions.forEach((definition) => {
             synonyms = [...synonyms, definition.synonyms];
@@ -31,7 +42,10 @@ const synonymsAPI = async (word) => {
 
 const filterWords = (clue) => {
     const words = clue
+        // Brackets
         .replace(/ *\([^)]*\) */g, '')
+        // Punctuation
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
         .trim()
         .split(/\s+/);
     const filtered = words.filter((w) => !ignorewords['words'].includes(w));
--- src/utils/parseClues/helperFunctions.js
+++ src/utils/parseClues/helperFunctions.js
@@ -1,10 +1,18 @@
-
-
 export function countWords(str) {
-	return str.trim().split(/\s+/).length;
+    return str.trim().split(/\s+/).length;
 }
 
 export function removePunctuation(clue) {
-	clue = clue.replace(/'/g, ' ');
-	return clue.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~]/g,"");
+    clue = clue.replace(/'/g, ' ');
+    return clue.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~]/g, '');
+}
+
+export function clueLength(clue) {
+    let split = clue.split('(');
+    const last = split[split.length - 1];
+    const numberPattern = /\d+/g;
+    const numbers = last.match(numberPattern);
+	 let num = 0;
+	 numbers.forEach(number => num += parseInt(number))
+	 return num;
 }
