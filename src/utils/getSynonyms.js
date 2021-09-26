fbf677cb07b7fcae52eafd46eb756a44f64cfb78
Niall Murphy <leturf@gmail.com>
2021-09-26 17:46:50 +0700

    Synonyms working. Will change to parent state.

--- src/utils/getSynonyms.js
+++ src/utils/getSynonyms.js
@@ -6,28 +6,49 @@ const getSynonyms = async (clue, setSynonyms) => {
     const words = filterWords(clue);
     const synonyms = {};
     const requests = [];
-    requests.push(
-        words.forEach(async (word) => {
-            const syns = await synonymsAPI(word);
-            if (syns.length > 0) {
-                const filtered = filterReturnedSynonyms(syns, clue);
-                if (filtered.length > 0) {
-                    synonyms[word] = filtered;
-                }
-            }
-        })
-    );
+    words.forEach(async (word) => {
+        requests.push(
+            axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        );
+    });
     axios
         .all(requests)
         .then(
             axios.spread((...responses) => {
-                console.log(responses);
+                responses.map((response) => {
+                    console.log(response);
+                    const syns = synonymsParse(response);
+                    if (syns.length > 0) {
+                        const filtered = filterReturnedSynonyms(syns, clue);
+                        if (filtered.length > 0) {
+                            synonyms[response.data[0].word] = filtered;
+                        }
+                    }
+                });
                 setSynonyms(synonyms);
             })
         )
         .catch(() => {
             console.log('There has been an error getting synonyms.');
         });
+};
+
+const synonymsParse = (response) => {
+	let synonyms = [];
+	if (!response) return;
+	response.data[0].meanings.forEach((meaning) => {
+		 if (
+			  meaning['partOfSpeech'] == 'noun' ||
+			  meaning['partOfSpeech'] == 'adjective'
+		 ) {
+			  meaning.definitions.forEach((definition) => {
+					synonyms = [...synonyms, definition.synonyms];
+			  });
+		 }
+	});
+	const merged = [].concat.apply([], synonyms);
+	console.log(merged)
+	return merged;
 };
 
 const filterReturnedSynonyms = (synonyms, clue) => {
@@ -41,35 +62,12 @@ const filterReturnedSynonyms = (synonyms, clue) => {
     });
 };
 
-const synonymsAPI = async (word) => {
-    const response = await axios
-        .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-        .catch((error) => {
-            return;
-        });
-
-    let synonyms = [];
-    if (!response) return;
-    response.data[0].meanings.forEach((meaning) => {
-        if (
-            meaning['partOfSpeech'] == 'noun' ||
-            meaning['partOfSpeech'] == 'adjective'
-        ) {
-            meaning.definitions.forEach((definition) => {
-                synonyms = [...synonyms, definition.synonyms];
-            });
-        }
-    });
-    const merged = [].concat.apply([], synonyms);
-    return merged;
-};
-
 const filterWords = (clue) => {
     const words = clue
         // Brackets
         .replace(/ *\([^)]*\) */g, '')
         // Punctuation
-        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+        .replace(/[.,\/#!$%\^&\*;:{}'=\-—_`~()]/g, '')
         .trim()
         .split(/\s+/);
     const filtered = words.filter((w) => !ignorewords['words'].includes(w));
