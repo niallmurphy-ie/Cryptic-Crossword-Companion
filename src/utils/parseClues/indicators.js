d0b1259492328b8e05bd86a919878d376360ed34
Niall Murphy <leturf@gmail.com>
2021-09-27 00:09:53 +0700

    Switch data to map

--- src/utils/parseClues/indicators.js
+++ src/utils/parseClues/indicators.js
@@ -1,18 +1,63 @@
 import { countWords } from './helperFunctions';
 // JSON
 import indicators from '../../data/indicators';
-import insertions from '../../data/insertions';
+import insertionWords from '../../data/insertionWords';
+import containmentWords from '../../data/containmentWords';
+import generalDeletionWords from '../../data/generalDeletionWords';
+import hiddenWords from '../../data/hiddenWords';
+import spoonerWords from '../../data/spoonerWords';
+import letterSwapWords from '../../data/letterSwapWords';
+import reverseLeftWords from '../../data/reverseLeftWords';
+import reverseUpwardsWords from '../../data/reverseUpwardsWords';
+
 // Check indicators
 function returnIndicators(clueText) {
     let returnArray = [];
-    // Go through object
+    /**
+     * Go through Indicators
+     *  */
+    // Insertions
     const returnedInsertions = parseIndicators(
         'Insertions',
         clueText,
-        insertions.words
+        insertionWords.words
     );
-    if (returnedInsertions)
-        returnArray = returnArray.concat(returnedInsertions);
+    if (returnedInsertions) returnArray.push(returnedInsertions);
+    // Containment
+    const containment = parseIndicators(
+        'Containment',
+        clueText,
+        containmentWords.words
+    );
+    if (containment) returnArray.push(containment);
+    // General Deletion
+    const deletion = parseIndicators(
+        'General Deletion',
+        clueText,
+        generalDeletionWords.words
+    );
+    if (deletion) returnArray.push(deletion);
+    // Hidden words
+    const hidden = parseIndicators('Hidden Words', clueText, hiddenWords.words);
+    if (hidden) returnArray.push(hidden);
+    // Spooner
+    const spooner = parseIndicators('Spooner', clueText, spoonerWords.words);
+    if (spooner) returnArray.push(spooner);
+    // Letter Swap
+    const swap = parseIndicators(
+        'Letter Swap / Movement',
+        clueText,
+        letterSwapWords.words
+    );
+    if (swap) returnArray.push(swap);
+    // Reverse Left
+    const left = parseIndicators(
+        'Reverse left',
+        clueText,
+        reverseLeftWords.words
+    );
+    if (left) returnArray.push(left);
+
     return returnArray;
 }
 
