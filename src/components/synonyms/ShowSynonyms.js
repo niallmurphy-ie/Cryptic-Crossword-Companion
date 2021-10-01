5583a3a21543a89356fd5661c228df3d2f64ec08
Niall Murphy <leturf@gmail.com>
2021-10-01 17:27:42 +0700

    Before inversing indicators

--- src/components/synonyms/ShowSynonyms.js
+++ src/components/synonyms/ShowSynonyms.js
@@ -1,50 +1,60 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const ShowSynonyms = ({ synonyms, handleLength }) => {
-	const synsArray = [];
-	// const length = synonyms.length;
-	const length = synonyms.synonymLength;
 
-	for (const s in synonyms.synonymData) {
-		 // Filter by length
-		 const synonymsArray = synonyms.synonymData[s].filter(
-			  // Replace punctuation so multiple words match length
-			  (syn) =>
-					syn.replace(/[.,\/#!$%\^&\*;:{}'=\-—_`~()]/g, '').length ===
-					length
-		 );
-		 // Sort by length
-		 synonymsArray.sort(function (a, b) {
-			  return a.length - b.length;
-		 });
-		 // Create fragment Array
-		 synsArray.push(
-			  <Fragment key={'synonym_' + synonyms[s]}>
-					<div className="crossword__clue__text">
-						 <strong>{s}: </strong> {synonymsArray.join(', ')}
-					</div>
-			  </Fragment>
-		 );
-	}
-	// Synonyms Length Text
-	let synonymsLengthText = '';
-	synonyms.synonymLength
-		 ? (synonymsLengthText = `Characters: ${synonyms.synonymLength}`)
-		 : (synonymsLengthText = 'Characters: 0');
+    const length = synonyms.synonymLength;
+    const synsArray = buildSynonyms(synonyms, length);
 
-	return (
-		 <div>
-			  <div>
-					<b>Synonyms: </b>
-					<button onClick={() => handleLength('decrease')}> - </button>
-					<button onClick={() => handleLength('increase')}> + </button>
-					<span> {synonymsLengthText}</span>
-			  </div>
-			  {synsArray.map((s) => (
-					<div>{s}</div>
-			  ))}
-		 </div>
-	);
+    if (!synsArray || synsArray.length === 0) return <div></div>;
+
+    // Synonyms Length Text
+    let synonymsLengthText = '';
+    synonyms.synonymLength
+        ? (synonymsLengthText = `Characters: ${synonyms.synonymLength}`)
+        : (synonymsLengthText = 'Characters: 0');
+
+    return (
+        <div>
+            <div>
+                Synonyms:
+                <button disabled={length < 2} onClick={() => handleLength('decrease')}> - </button>
+                <button onClick={() => handleLength('increase')}> + </button>
+                <span> {synonymsLengthText}</span>
+            </div>
+            {synsArray.map((s) => (
+                <div>{s}</div>
+            ))}
+        </div>
+    );
+};
+
+const buildSynonyms = (synonyms, length) => {
+    if (!length) return <div></div>;
+
+    const synsArray = [];
+
+    for (const s in synonyms.synonymData) {
+        // Filter by length
+        const synonymsArray = synonyms.synonymData[s].filter(
+            // Replace punctuation so multiple words match length
+            (syn) =>
+                syn.replace(/[.,\/#!$%\^&\*;:{}'=\-—_`~()]/g, '').length ===
+                length
+        );
+        // Sort by length
+        synonymsArray.sort(function (a, b) {
+            return a.length - b.length;
+        });
+        // Create fragment Array
+        synsArray.push(
+            <Fragment key={'synonym_' + synonyms[s]}>
+                <div className="crossword__clue__text">
+                    <strong>{s}: </strong> {synonymsArray.join(', ')}
+                </div>
+            </Fragment>
+        );
+    }
+    return synsArray;
 };
 
 export default ShowSynonyms;
