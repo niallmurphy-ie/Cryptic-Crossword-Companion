086ac2e8b8a2d22086ecd86aa1fc59fd53b6f841
Niall Murphy <leturf@gmail.com>
2022-02-04 17:04:54 +0700

    Working again

--- src/public/background.js
+++ src/public/background.js
@@ -6,24 +6,13 @@ const allowedURLs = [
 ];
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  console.log(tabId);
 	if (
 		changeInfo.status === 'complete' &&
 		allowedURLs.some((url) => tab.url.includes(url))
-		// && !document.querySelector('#cryptic_crossword_help')
 	) {
-		chrome.scripting.executeScript(
-			{ target: { tabId }, files: ['./inject_script.js'] },
-
-			function () {
-				chrome.scripting.executeScript(
-					tabId,
-					{ injection: './foreground.bundle.js' },
-					function () {
-						console.log('INJECTED AND EXECUTED');
-					}
-				);
-			}
-		);
+		chrome.scripting.executeScript({
+			target: { tabId },
+			files: ['./inject_script.js', './foreground.bundle.js'],
+		});
 	}
 });
