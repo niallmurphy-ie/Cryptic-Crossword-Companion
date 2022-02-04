330bd00e2eacbd5a10362504c131b672b5553038
Niall Murphy <leturf@gmail.com>
2022-02-04 16:57:30 +0700

    tabID working and some change showing

--- src/public/background.js
+++ src/public/background.js
@@ -1,28 +1,29 @@
 // Script only runs on crossword pages
 const allowedURLs = [
-  "https://www.theguardian.com/crosswords/quiptic/",
-  "https://www.theguardian.com/crosswords/cryptic/",
-  "https://www.theguardian.com/crosswords/everyman/"
+	'https://www.theguardian.com/crosswords/quiptic/',
+	'https://www.theguardian.com/crosswords/cryptic/',
+	'https://www.theguardian.com/crosswords/everyman/',
 ];
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (
-    changeInfo.status === "complete" &&
-    allowedURLs.some((url) => tab.url.includes(url)) &&
-    !document.querySelector('#cryptic_crossword_help')
-  ) {
-    chrome.tabs.executeScript(
-      tabId,
-      { file: "./inject_script.js" },
-      function () {
-        chrome.tabs.executeScript(
-          tabId,
-          { file: "./foreground.bundle.js" },
-          function () {
-            console.log("INJECTED AND EXECUTED");
-          }
-        );
-      }
-    );
-  }
+  console.log(tabId);
+	if (
+		changeInfo.status === 'complete' &&
+		allowedURLs.some((url) => tab.url.includes(url))
+		// && !document.querySelector('#cryptic_crossword_help')
+	) {
+		chrome.scripting.executeScript(
+			{ target: { tabId }, files: ['./inject_script.js'] },
+
+			function () {
+				chrome.scripting.executeScript(
+					tabId,
+					{ injection: './foreground.bundle.js' },
+					function () {
+						console.log('INJECTED AND EXECUTED');
+					}
+				);
+			}
+		);
+	}
 });
